351c258ba219e3c15235728157b3d86622f8e2e5
崔超然 <915422643@qq.com>
2024-10-13 17:55:22 +0800

    update

--- README.md
+++ README.md
@@ -1,55 +1,48 @@
 # Vscode + Cmake
 
 ```markdown
-Linux、Mac下：gcc/g++、gdb、makefile、cmake 等命令在使用之前需要先安装，安装方法请自行搜索。
-Windows下：需要安装 MinGW 和 cmake，还要配置环境变量
+Linux、Mac 下：gcc/g++、gdb、makefile、cmake 等命令在使用之前需要先安装，安装方法请自行搜索。
+Windows 下：需要安装 MinGW 和 cmake，还要配置环境变量
 ```
 
-从Vscode中打开项目根目录
+从 Vscode 中打开项目根目录
 
-1. 在Vscode中安装插件
+1. 在 Vscode 中安装插件
 
-   * **C/C++** : Micorsoft, C/C++ IntelliSense, debugging, and code browsing
-   * **Cmake** : twxs, CMake langage support for Visual Studio Code
-   * **Cmake Tools** : Micorsoft, Extended Cmake support in Visual Studio Code
+   - **C/C++** : Micorsoft, C/C++ IntelliSense, debugging, and code browsing
+   - **Cmake** : twxs, CMake langage support for Visual Studio Code
+   - **Cmake Tools** : Micorsoft, Extended Cmake support in Visual Studio Code
 
-2. 编译工具选择(select kits)    [（参考网站）](https://zhuanlan.zhihu.com/p/52874931) 
+2. 编译工具选择(select kits) [（参考网站）](https://zhuanlan.zhihu.com/p/52874931)
 
-   win下Ctrl + Shift + P，(或左下角齿轮 --> 选择Command Palette...)，输入cmake:Select a Kit，选择安装的GCC 8.0.1(即安装的 MinGW 路径下的编译器)
+   win 下 Ctrl + Shift + P，(或左下角齿轮 --> 选择 Command Palette...)，输入 cmake:Select a Kit，选择安装的 GCC 8.0.1(即安装的 MinGW 路径下的编译器)
 
 3. 配置(configure)
 
-   等同于执行`cmake ..`，可选择Debug、Release等等，也可以自己自定义配置
+   等同于执行`cmake ..`，可选择 Debug、Release 等等，也可以自己自定义配置
 
 4. 编译(build)
    等同于执行`make`，选择编译目标
 
 5. 调试(debug)
-   调用gdb进行调试，调试目标选择
+   调用 gdb 进行调试，调试目标选择
 
-
-
-
-
-
-
-
-### cmake，make流程:
+## cmake，make 流程
 
 ```markdown
 CMakeLists.txt -(configure)-> CmakeCache.txt -(generate)-> Makefile
 CMakeLists.txt -----------------cmake--------------------- Makefile ----make----> executalbe file
 ```
 
-### 如何调试(Debug)工程
+## 如何调试(Debug)工程
 
-在CMakeLists.txt中添加Debug参数：
+在 CMakeLists.txt 中添加 Debug 参数：
 
 ```shell
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
 ```
 
-若调试工程需要传入参数，在对应工程建立.vscode文件夹settings.json文件里，添加如下：
+若调试工程需要传入参数，在对应工程建立.vscode 文件夹 settings.json 文件里，添加如下：
 
 ```shell
 {
@@ -63,21 +56,21 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
 }
 ```
 
-不要添加在系统settings.json(C:\Users\cui\AppData\Roaming\Code\User\settings.json)里，会影响其他工程。
+不要添加在系统 settings.json(C:\Users\cui\AppData\Roaming\Code\User\settings.json)里，会影响其他工程。
 
-### VsCode 指定默认Generator：
+## VsCode 指定默认 Generator
 
-在Windows下，默认的Generator为自带的nmake，同一个项目转移到Linux下时就会比较麻烦。
-**方法1：**
+在 Windows 下，默认的 Generator 为自带的 nmake，同一个项目转移到 Linux 下时就会比较麻烦。
+**方法 1：**
 
 ```shell
 cmake -G "MinGW MakeFiles" ..
 ```
 
-即每次cmake时都要指定 -G 参数
+即每次 cmake 时都要指定 -G 参数
 
-**方法2：**
-在每个CMakeList.txt所在目录建立Preload.cmake文件，以txt形式打开，添加以下内容
+**方法 2：**
+在每个 CMakeList.txt 所在目录建立 Preload.cmake 文件，以 txt 形式打开，添加以下内容
 
 ```tst
 set(CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
@@ -85,20 +78,18 @@ set(CMAKE_GENERATOR "MinGW Makefiles" CACHE INTERNAL "" FORCE)
 
 （这种方法未在官网给出，[此网页](https://stackoverflow.com/questions/11269833/cmake-selecting-a-generator-within-cmakelists-txt)找到）
 
-**方法3：**
-若在CMakeLists.txt中设置`set(CMAKE_GENERATOR "MinGW MakeFiles")`，第一次cmake会使用默认Generator，此时Generator在CMakeCache.txt中被设置，之后就会使用指定的Generator。但此方法要输入两边cmake，有点麻烦    (win下尝试不可用)
+**方法 3：**
+若在 CMakeLists.txt 中设置`set(CMAKE_GENERATOR "MinGW MakeFiles")`，第一次 cmake 会使用默认 Generator，此时 Generator 在 CMakeCache.txt 中被设置，之后就会使用指定的 Generator。但此方法要输入两边 cmake，有点麻烦 (win 下尝试不可用)
 
 cmake 支持 Generator 可参考网站:
-https://cmake.org/cmake/help/latest/manual/cmake-generators.7.html
+<https://cmake.org/cmake/help/latest/manual/cmake-generators.7.html>
 
+## 参考网站
 
+[1] <https://blog.csdn.net/u014265289/article/details/78213643/?utm_medium=distribute.pc_relevant.none-task-blog-baidujs-2>
 
-### 参考网站
+[2] <https://zhuanlan.zhihu.com/p/52874931>
 
-[1] https://blog.csdn.net/u014265289/article/details/78213643/?utm_medium=distribute.pc_relevant.none-task-blog-baidujs-2
+[3] <https://butui.me/post/debug-with-vscode-and-cmake/>
 
-[2] https://zhuanlan.zhihu.com/p/52874931
-
-[3] https://butui.me/post/debug-with-vscode-and-cmake/
-
-[4] https://www.jianshu.com/p/9beafc25f27f
+[4] <https://www.jianshu.com/p/9beafc25f27f>
--- main.cpp
+++ main.cpp
@@ -1,186 +1,28 @@
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <map>
-#include <sstream>
-#include <string>
-#include <vector>
+#include <optional>
 
-using namespace std;
-
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-<<<<<<< HEAD
-// 递归由数组构建完全/满二叉树
-=======
-// 由数组构建完全/满二叉树
->>>>>>> 00483d32a1f382d95fbc7b5532cad8f36527b7d7
-TreeNode *constructBinaryTree(const vector<string> &vecstr, int i) {
-  if (vecstr.empty())
-    return nullptr;  // 特殊情况处理
-  if (i >= vecstr.size())
-    return nullptr;
-  TreeNode *root = nullptr;
-  if (vecstr[i] != "null") {
-    root = new TreeNode(stoi(vecstr[i]));
-    root->left = constructBinaryTree(vecstr, 2 * i + 1);
-    root->right = constructBinaryTree(vecstr, 2 * i + 2);
-  }
-  return root;
+std::optional<int> divide(int a, int b) {
+    if (b == 0) {
+        return std::nullopt;
+    } else {
+        return a / b;
+    }
 }
 
-class Solution {
- public:
-  map<int, int> mapDepth;
-  vector<int> depth;
-  int cnt = 1;
-  // void getDepth(TreeNode *root) {
-  //   if (root == nullptr) return;
-  //   depth.push_back(cnt);
-  //   cnt++;
-  //   getDepth(root->left);
-  //   getDepth(root->right);
-  //   cnt--;
-  // }
-
-<<<<<<< HEAD
-  void getDepth(const TreeNode *root, int cntDepth) {
-=======
-  void getDepth(TreeNode *root, int cntDepth) {
->>>>>>> 00483d32a1f382d95fbc7b5532cad8f36527b7d7
-    if (root == nullptr) {
-      return;
-    }
-    // mapDepth[root->val] = max(mapDepth[root->val], cntDepth); // 键值不存在，[]访问插入默认值；此处可用，不建议
-    pair<map<int, int>::iterator, bool> ret = mapDepth.insert(pair<int, int>(root->val, cntDepth));
-    // 已存在key，取最大val
-    if (!ret.second) {
-      (ret.first)->second = max((ret.first)->second, cntDepth);
-    }
-    getDepth(root->left, cntDepth + 1);
-    getDepth(root->right, cntDepth + 1);
-<<<<<<< HEAD
-  }
-
-  vector<int> ValueDepth(const vector<int> &target, const TreeNode *root) {
-    if (!root) {
-      return {};
-    }
-
-    // 获取深度值
-    getDepth(root, 1);
-
-    // 一次反向遍历，获取key大于当前key的节点的最大深度值
-    auto it1 = mapDepth.rbegin();
-    auto it2 = it1;
-    for (it1++; it1 != mapDepth.rend(); ++it1, ++it2) {
-      it1->second = max(it1->second, (it2)->second);
-    }
-
-    // for (auto it1 = mapDepth.rbegin(); it1 != mapDepth.rend(); ++it1) {
-    //   it1->second = max(it1->second, 10);
-    // }
-
-    vector<int> retVec;
-    for (const int &x : target) {
-      auto iter = mapDepth.lower_bound(x);
-      if (iter != mapDepth.end()) {
-        retVec.push_back(iter->second);
-      } else {
-        retVec.push_back(-1);
-      }
-    }
-    return retVec;
-=======
-  }
-
-  vector<int> ValueDepth(const vector<int> &target, const TreeNode *root) {
-    if (!root) {
-      return {};
-    }
-    // 一次反向遍历，获取key大于当前key的节点的最大深度值
-    // for (auto it = mapDepth.rbegin() + 1; it != mapDepth.rend(); ++it) {
-    //     it->second = max(it->second, (it + 1)->second);
-    // }
-    // 一次反向遍历，获取key大于当前key的节点的最大深度值
-    auto it1 = mapDepth.rbegin();
-    auto it2 = it1;
-    for (it1++; it1 != mapDepth.rend(); ++it1, ++it2) {
-      it1->second = max(it1->second, (it2)->second);
-    }
-
-    vector<int> retVec;
-    for (const int &x : target) {
-      retVec.push_back(mapDepth.lower_bound(x)->second);
-    }
-    return retVec;
-  }
-
-  int maxDepth(TreeNode *root) {
-    if (!root) {
-      return 0;  // 特殊情况处理
-    }
-    getDepth(root, 1);
-    sort(depth.begin(), depth.end(), less<int>());
-    return depth.back();
->>>>>>> 00483d32a1f382d95fbc7b5532cad8f36527b7d7
-  }
-};
-
 int main() {
-  Solution sol;
+    auto result1 = divide(10, 2);
+    if (result1.has_value()) {
+        std::cout << "Result 1: " << result1.value() << std::endl;
+    } else {
+        std::cout << "Result 1: division by zero" << std::endl;
+    }
 
-  stringstream sstr;
-<<<<<<< HEAD
-  sstr << "3,4,11,3,null,null,8,7,null,null,null,null,null,5";
-=======
-  sstr << "3,4,11,3,null,null,8,7,null,5";
->>>>>>> 00483d32a1f382d95fbc7b5532cad8f36527b7d7
-  vector<string> vec;
-  string s;
-  while (getline(sstr, s, ',')) {
-    vec.push_back(s);
-  }
-  for_each(vec.begin(), vec.end(), [](const string str) { cout << str << endl; });
-  cout << "vec size: " << vec.size() << endl;
-  int treeDepth = log2(vec.size() + 1);
-  cout << "treeDepth: " << treeDepth << endl;
+    auto result2 = divide(10, 0);
+    if (result2.has_value()) {
+        std::cout << "Result 2: " << result2.value() << std::endl;
+    } else {
+        std::cout << "Result 2: division by zero" << std::endl;
+    }
 
-  TreeNode *root = constructBinaryTree(vec, 0);  // 递归构建完全/满二叉树
-
-<<<<<<< HEAD
-  sstr.clear();
-  sstr << "6,2,9,7,9";
-  vector<int> target;
-  while (getline(sstr, s, ',')) {
-    target.push_back(stoi(s));
-  }
-  // for_each(target.begin(), target.end(), [](const int x) { cout << x << endl; });
-
-  vector<int> ans = sol.ValueDepth(target, root);
-  for_each(ans.begin(), ans.end(), [](const int x) { cout << x << endl; });
-
-=======
-  // sol.getDepth(root, 1);
-  // cout << sol.maxDepth(root) << "====" << endl;
-
-  ;
-  sstr.clear();
-  sstr << "6,2,9,7,9";
-  vector<int> target;
-  while (getline(sstr, s, ',')) {
-    target.push_back(stoi(s));
-  }
-  for_each(target.begin(), target.end(), [](const int x) { cout << x << endl; });
-  // vector<int> ans = sol.ValueDepth(target, root);
-  // for_each(ans.begin(), ans.end(), [](const int x) { cout << x << endl; });
->>>>>>> 00483d32a1f382d95fbc7b5532cad8f36527b7d7
-  return 0;
+    return 0;
 }
